c3358fe9de290753a55e29881d03339dc07aa140
Paul Milham <domreinalabaster@gmail.com>
2014-01-16 22:06:21 -0700

    Added multitouch support to PointerInput.js :)

--- scene/system/PointerInput.js
+++ scene/system/PointerInput.js
@@ -2,19 +2,20 @@ Plx.PointerInput = function() {
   Plx.System.call(this);
   this.componentTypes = [Plx.Tappable, Plx.Draggable];
   this.pointerComponents = [];
-
-  this.mouseDown = false;
-  this.mouseLoc = new Plx.Point();
-  this.mouseTarget = null;
+  // this.pointers is mouse and touch data combined
+  this.pointers = {};
 
   // controls
   var _this = this;
   this.mouseDownFunc = function(event){_this.onMouseDown(event)};
   this.mouseUpFunc = function(event){_this.onMouseUp(event)};
   this.mouseMoveFunc = function(event){_this.onMouseMove(event)};
+  
   this.touchStartFunc = function(event){_this.onTouchStart(event)};
   this.touchEndFunc = function(event){_this.onTouchEnd(event)};
   this.touchMoveFunc = function(event){_this.onTouchMove(event)};
+  this.touchCancelFunc = function(event){_this.onTouchCancel(event)};
+  this.touchLeaveFunc = function(event){_this.onTouchLeave(event)};
   // mouse events
   document.getElementById("canvas").addEventListener("mousedown", this.mouseDownFunc, false);
   document.getElementById("canvas").addEventListener("mouseup", this.mouseUpFunc, false);
@@ -23,6 +24,8 @@ Plx.PointerInput = function() {
   document.getElementById("canvas").addEventListener("touchstart", this.touchStartFunc, false);
   document.getElementById("canvas").addEventListener("touchend", this.touchEndFunc, false);
   document.getElementById("canvas").addEventListener("touchmove", this.touchMoveFunc, false);
+  document.getElementById("canvas").addEventListener("touchcancel", this.touchCancelFunc, false);
+  document.getElementById("canvas").addEventListener("touchleave", this.touchLeaveFunc, false);
 };
 
 Plx.PointerInput.prototype = Object.create(Plx.System.prototype);
@@ -42,77 +45,108 @@ Plx.PointerInput.prototype.removeComponent = function(component) {
     this.pointerComponents.splice(index, 1);
 };
 
+// TODO: if we scale the game we probably need to scale these inputs too (in the opposite direction though)
 Plx.PointerInput.prototype.onMouseDown = function(event) {
-  // TODO: if we scale the game we probably need to scale these inputs too (in the opposite direction though)
   this.mouseDown = true;
+  this.pointerStart("mouse", event.layerX, event.layerY);
+};
+
+Plx.PointerInput.prototype.onMouseUp = function(event) {
+  this.mouseDown = false;
+  this.pointerEnd("mouse");
+};
+
+Plx.PointerInput.prototype.onMouseMove = function(event) {
+  if (this.mouseDown)
+    this.pointerMove("mouse", event.layerX, event.layerY);
+};
+
+// NOTE: layerX,Y is relative to the element, clientX,Y is relative to the document make these compatible (they just so hapen to be the same in thie case)
+Plx.PointerInput.prototype.onTouchStart = function(event) {
+  event.preventDefault(); // This is a hack so that Android dispatches the touchend event (I guess it also disables native scrolling) I guess this also prevents the mouse event from being sent
+  for (var i = 0; i < event.changedTouches.length; i ++) {
+    var touch = event.changedTouches[i];
+    this.pointerStart(touch.identifier, touch.clientX, touch.clientY);
+  }
+};
+
+Plx.PointerInput.prototype.onTouchEnd = function(event) {
+  event.preventDefault();
+  for (var i = 0; i < event.changedTouches.length; i ++) {
+    var touch = event.changedTouches[i];
+    this.pointerEnd(touch.identifier);
+  }
+};
+
+Plx.PointerInput.prototype.onTouchCancel = function(event) {
+  this.onTouchEnd(event);
+};
+
+Plx.PointerInput.prototype.onTouchLeave = function(event) {
+  this.onTouchEnd(event);
+};
+
+Plx.PointerInput.prototype.onTouchMove = function(event) {
+  event.preventDefault();
+  for (var i = 0; i < event.changedTouches.length; i ++) {
+    var touch = event.changedTouches[i];
+    this.pointerMove(touch.identifier, touch.clientX, touch.clientY);
+  }
+};
+
+Plx.PointerInput.prototype.pointerStart = function(id, x, y) {
+  var pointer = this.pointers[id] = {x: x, y: y, target: null};
   for (var i = 0; i < this.pointerComponents.length; i ++) {
     var pointerComponent = this.pointerComponents[i];
     if (!pointerComponent.enabled)
       continue;
-    if (pointerComponent.collisionCheck(event.layerX, event.layerY)) {
+    if (pointerComponent.collisionCheck(pointer.x, pointer.y)) {
       pointerComponent.beacon.emit("tapped", null);
       pointerComponent.beacon.emit("entered", null);
-      this.mouseTarget = pointerComponent;
+      pointer.target = pointerComponent;
       break;
     }
   }
 };
 
-Plx.PointerInput.prototype.onMouseUp = function(event) {
-  if (this.mouseTarget) {
-    this.mouseTarget.beacon.emit("exited", null);
-    this.mouseTarget = null;
+Plx.PointerInput.prototype.pointerEnd = function(id) {
+  var pointer = this.pointers[id];
+  if (!pointer)
+    return;
+  if (pointer.target) {
+    pointer.target.beacon.emit("lifted", null);
+    pointer.target.beacon.emit("exited", null);
   }
-  this.mouseDown = false;
+  delete this.pointers[id];
 };
 
-Plx.PointerInput.prototype.onMouseMove = function(event) {
-  if (this.mouseTarget) {
-    if (this.mouseTarget.collisionCheck(event.layerX, event.layerY)) {
+Plx.PointerInput.prototype.pointerMove = function(id, x, y) {
+  var pointer = this.pointers[id];
+  if (!pointer) {
+    pointer = this.pointers[id] = {x: x, y: y, target: null};
+  }
+  pointer.x = x;
+  pointer.y = y;
+  if (pointer.target) {
+    if (pointer.target.collisionCheck(pointer.x, pointer.y)) {
     }
     else {
-      this.mouseTarget.beacon.emit("exited", null);
-      this.mouseTarget = null;
+      pointer.target.beacon.emit("exited", null);
+      pointer.target = null;
     }
   }
-  else if (this.mouseDown) {
+  else {
     for (var i = 0; i < this.pointerComponents.length; i ++) {
       var pointerComponent = this.pointerComponents[i];
       if (!pointerComponent.enabled)
         continue;
-      if (pointerComponent.collisionCheck(event.layerX, event.layerY)) {
+      if (pointerComponent.collisionCheck(pointer.x, pointer.y)) {
         pointerComponent.beacon.emit("entered", null);
-        this.mouseTarget = pointerComponent;
+        pointer.target = pointerComponent;
         break;
       }
     }
   }
-};
-
-// NOTE: layerX,Y is relative to the element, clientX,Y is relative to the document make these compatible (they just so hapen to be the same in thie case)
-Plx.PointerInput.prototype.onTouchStart = function(event) {
-  // window.alert 'touchstart'
-  event.preventDefault(); // This is a hack so that Android dispatches the touchend event (I guess it also disables native scrolling) I guess this also prevents the mouse event from being sent
-  // window.alert "touches.length //{event.touches.length}"
-  touch = event.touches[0];
-  // window.alert "touchX: //{touch.clientX}, touchY: //{touch.clientY}, "
-  this.beacon.emit("mouseDown", {x:touch.clientX, y:touch.clientY}); // TODO: if we scale the game we probably need to scale these inputs too (in the opposite direction though)
-};
-
-Plx.PointerInput.prototype.onTouchEnd = function(event) {
-  // window.alert 'touchend'
-  // window.alert "touches.length //{event.touches.length}"
-  event.preventDefault();
-  this.beacon.emit("mouseUp", null); // TODO: if we scale the game we probably need to scale these inputs too (in the opposite direction though)
-};
-
-Plx.PointerInput.prototype.onTouchMove = function(event) {
-  // window.alert 'touchmove'
-  // window.alert "touches.length //{event.touches.length}"
-  event.preventDefault();
-  touch = event.touches[0];
-  // window.alert "touchX: //{touch.clientX}, touchY: //{touch.clientY}, "
-  this.beacon.emit("mouseMove", {x:touch.clientX, y:touch.clientY}); // TODO: if we scale the game we probably need to scale these inputs too (in the opposite direction though)
 };
 
 Plx.PointerInput.prototype.destroy = function() {
@@ -120,7 +154,9 @@ Plx.PointerInput.prototype.destroy = function() {
   document.getElementById("canvas").removeEventListener("mouseup", this.mouseUpFunc, false);
   document.getElementById("canvas").removeEventListener("mousemove", this.mouseMoveFunc, false);
 
-  document.getElementById("canvas").addEventListener("touchstart", this.touchStartFunc, false);
-  document.getElementById("canvas").addEventListener("touchend", this.touchEndFunc, false);
-  document.getElementById("canvas").addEventListener("touchmove", this.touchMoveFunc, false);
+  document.getElementById("canvas").removeEventListener("touchstart", this.touchStartFunc, false);
+  document.getElementById("canvas").removeEventListener("touchend", this.touchEndFunc, false);
+  document.getElementById("canvas").removeEventListener("touchmove", this.touchMoveFunc, false);
+  document.getElementById("canvas").removeEventListener("touchcancel", this.touchCancelFunc, false);
+  document.getElementById("canvas").removeEventListener("touchleave", this.touchLeaveFunc, false);
 };
